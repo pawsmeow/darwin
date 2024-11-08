0bdab2b52577cfa54b0ab6576c172d4464f6c6bc
pawsmeow <161309672+pawsmeow@users.noreply.github.com>
2024-11-08 11:48:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("tournament-form");
+    const submitButton = document.getElementById("submit-button");
+
+    // Telegram Web App API
+    const tg = window.Telegram.WebApp;
+    tg.expand(); // Расширить приложение на весь экран
+
+    submitButton.addEventListener("click", () => {
+        const tournamentData = {
+            gameid: document.getElementById("tournament_creator").value,
+            nickname: document.getElementById("tournament_name").value,
+            username: document.getElementById("tournament_about").value,
+            web_app_type: "new_tur"
+        };
+
+        // Отправка данных боту
+        tg.sendData(JSON.stringify(tournamentData)); // Отправляем данные WebAppData
+        tg.close(); // Закрыть Web App после отправки
+    });
+});
